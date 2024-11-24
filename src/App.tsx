9d345e1f57e6955dd245c249dec9843e219d6a50
devyn-miller <144622580+devyn-miller@users.noreply.github.com>
2024-11-24 12:16:23 -0800

    footer w

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import GameBoard from './components/GameBoard';
+import Footer from './components/Footer';
 
 function App() {
-  return <GameBoard />;
+  return (
+    <div className="min-h-screen">
+      <GameBoard />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
